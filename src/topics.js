3b722bf201e7d5251c2c5a071b53435f5286af77
Sagi Kedmi <git@sagi.io>
2022-03-12 09:15:29 +0200

    update prettier

--- src/topics.js
+++ src/topics.js
@@ -7,22 +7,26 @@ export const list = (baseInputs) => async () => {
   return topics;
 };
 
-export const get = (baseInputs) => async ({ topic }) => {
-  const { headers, projectId, baseUrl } = baseInputs;
-  const method = 'GET';
-  const url = `${baseUrl}/projects/${projectId}/topics/${topic}`;
-  const response = await fetch(url, { method, headers });
-  return await response.json();
-};
+export const get =
+  (baseInputs) =>
+  async ({ topic }) => {
+    const { headers, projectId, baseUrl } = baseInputs;
+    const method = 'GET';
+    const url = `${baseUrl}/projects/${projectId}/topics/${topic}`;
+    const response = await fetch(url, { method, headers });
+    return await response.json();
+  };
 
-export const publish = (baseInputs) => async ({ topic, messages }) => {
-  const { headers, projectId, baseUrl } = baseInputs;
+export const publish =
+  (baseInputs) =>
+  async ({ topic, messages }) => {
+    const { headers, projectId, baseUrl } = baseInputs;
 
-  const method = 'POST';
-  const url = `${baseUrl}/projects/${projectId}/topics/${topic}:publish`;
-  const bodyJSON = { messages };
-  const body = JSON.stringify(bodyJSON);
+    const method = 'POST';
+    const url = `${baseUrl}/projects/${projectId}/topics/${topic}:publish`;
+    const bodyJSON = { messages };
+    const body = JSON.stringify(bodyJSON);
 
-  const response = await fetch(url, { method, headers, body });
-  return await response.json();
-};
+    const response = await fetch(url, { method, headers, body });
+    return await response.json();
+  };
